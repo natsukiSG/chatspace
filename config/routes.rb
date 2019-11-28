6e7d131af4e0448e69d3d7e1c8cc785cbd2d7114
natsukiSG <natsukineria@gmail.com>
2019-11-28 22:14:03 +0900

    front-discard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root to: "messages#index"
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
-  resources :messages, only: [:index, :create, :destroy]
-  end
+
 end
