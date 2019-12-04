d84f9c2f59f26c73a80998fac4fb1aba5ba1486d
natsukiSG <natsukineria@gmail.com>
2019-12-04 19:34:33 +0900

    非同期通信による修正

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(*****).on(*****, function(){
+      e.preventDefault()
+      $.ajax({
+        url: 取得したリクエストURL,  //同期通信でいう『パス』
+        type: 'POST',  //同期通信でいう『HTTPメソッド』
+        data: 取得したFormData,  
+        dataType: 'json',
+        processData: false,
+        contentType: false
+    })
+  })
